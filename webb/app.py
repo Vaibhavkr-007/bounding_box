2f0be62ea2031206a38f5f044f903c6beb205f09
Vaibhavkr07 <vk829301@gmail.com>
2024-10-24 17:44:04 +0530

    77

--- webb/app.py
+++ webb/app.py
@@ -76,5 +76,5 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8005)
+    app.run(debug=True,host=0.0.0.0, port=8005)
 
